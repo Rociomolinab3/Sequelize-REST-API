c52666a6325082d5b2124d0aa6451ea71dab76d1
Rocio <rociomolina.b7@gmail.com>
2024-01-12 13:53:03 -0300

    "landing resposive ok"

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -2,10 +2,26 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {fontFamily: {
-      'sans': ['Jost', 'sans-serif'],
-      'serif': ['Playfair Display', 'serif'],
-    },},
+    extend: {
+      fontFamily: {
+        'roboto_cond':['roboto_cond','roboto_cond'],
+        'roboto':['Roboto','sans-serif']
+      },
+      colors: {
+        'primary': '#cc0972',
+        'secondary': '#6d6e70',
+        'yellow': '#f49e27',
+        'green': '#67c1b8',
+        'violet': '#6d2c66',
+        'blue': '#252b5f',
+        'light': '#e9e9e9',
+        'dark': '#222222',
+      },
+      backgroundImage:{
+        'fondo': "url('https://firebasestorage.googleapis.com/v0/b/web-upro.appspot.com/o/preinscripciones%2FIlustraciones%20Landing_Mesa%20de%20trabajo%201.svg?alt=media&token=d2234cda-6851-486c-88aa-ad3d424c7106')",
+      }
+      
+    },
   },
   plugins: [],
 }
